d528176a7f61b4fede6a44e0577702b26e8eaed0
LeoDuarte <leonardo.duarte@maps.com.br>
2024-09-16 10:57:22 -0300

    First commit

---
+++ src/main/java/dev/leoduarte/kafkaprodcons/KafkaProdConsApplication.java
@@ -0,0 +1,13 @@
+package dev.leoduarte.kafkaprodcons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProdConsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProdConsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/leoduarte/kafkaprodcons/KafkaProdConsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leoduarte.kafkaprodcons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProdConsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
